f15235267b8824093d04e0ad87565d18dd726579
Daniel Sasser <danielbsasser@gmail.com>
2020-03-11 23:36:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -29,3 +29,5 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
+
+INSERT INTO pledges (amount, project_id, user_id) VALUES (10.00, 1, 2)
