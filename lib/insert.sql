6d732623861d2ceec959a05e0427a66602efcc40
Daniel Sasser <danielbsasser@gmail.com>
2020-03-11 23:34:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -18,3 +18,14 @@ INSERT INTO users (name, age) VALUES ("Aname", 21);
 INSERT INTO users (name, age) VALUES ("Aname", 21);
 INSERT INTO users (name, age) VALUES ("Aname", 21);
 INSERT INTO users (name, age) VALUES ("Aname", 21);
+
+INSERT INTO projects (title, category, funing_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
+INSERT INTO projects (title, category, funing_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
+INSERT INTO projects (title, category, funing_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
+INSERT INTO projects (title, category, funing_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
+INSERT INTO projects (title, category, funing_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
+INSERT INTO projects (title, category, funing_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
+INSERT INTO projects (title, category, funing_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
+INSERT INTO projects (title, category, funing_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
+INSERT INTO projects (title, category, funing_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
+INSERT INTO projects (title, category, funing_goal, start_date, end_date) VALUES ('A title', 'A category', 500.00, '01-01-01', '01-02-01');
